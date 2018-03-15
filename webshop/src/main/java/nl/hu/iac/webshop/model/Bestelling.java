856a61a7a6077e619964047091e31fe1df6337d3
Chankie <chanwai6@hotmail.com>
2018-03-15 14:32:57 +0100

    meta-inf toegevoegd met baseDAO en db connectie

--- webshop/src/main/java/nl/hu/iac/webshop/model/Bestelling.java
+++ webshop/src/main/java/nl/hu/iac/webshop/model/Bestelling.java
@@ -10,4 +10,28 @@ public class Bestelling {
 		this.adres_id = adres_id;
 		this.bestellingsregel_id = bestellingsregel_id;
 	}
+
+	public int getBestellingId() {
+		return bestelling_id;
+	}
+
+	public void setBestellingId(int bestelling_id) {
+		this.bestelling_id = bestelling_id;
+	}
+
+	public Adres getAdresId() {
+		return adres_id;
+	}
+
+	public void setAdresId(Adres adres_id) {
+		this.adres_id = adres_id;
+	}
+
+	public Bestellingsregel getBestellingsregel_id() {
+		return bestellingsregel_id;
+	}
+
+	public void setBestellingsregel_id(Bestellingsregel bestellingsregel_id) {
+		this.bestellingsregel_id = bestellingsregel_id;
+	}
 }
