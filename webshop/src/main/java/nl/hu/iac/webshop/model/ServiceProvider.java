52426f824dc39bbe0c4953e53bb498b7059418a9
donald <donald061098@hotmail.com>
2018-03-16 12:32:02 +0100

    adresresource,service toegevoegd

--- webshop/src/main/java/nl/hu/iac/webshop/model/ServiceProvider.java
+++ webshop/src/main/java/nl/hu/iac/webshop/model/ServiceProvider.java
@@ -2,8 +2,12 @@ package nl.hu.iac.webshop.model;
 
 public class ServiceProvider {
 	private static WebshopService webshopService = new WebshopService();
+	private static WebshopServiceKlanten webshopServiceKlanten = new WebshopServiceKlanten();
 	
 	public static WebshopService getWebshopService() {
 		return webshopService;
+	}
+	public static WebshopServiceKlanten getWebshopServiceKlanten() {
+		return webshopServiceKlanten;
 	}
 }
--- webshop/src/main/java/nl/hu/iac/webshop/webservices/AdresResource.java
+++ webshop/src/main/java/nl/hu/iac/webshop/webservices/AdresResource.java
@@ -1,5 +1,40 @@
 package nl.hu.iac.webshop.webservices;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import nl.hu.iac.webshop.model.Adres;
+import nl.hu.iac.webshop.model.ServiceProvider;
+import nl.hu.iac.webshop.model.WebshopServiceKlanten;
+import nl.hu.iac.webshop.persistence.AdresDAO;
+
+
+@Path("/adres")
 public class AdresResource {
+	AdresDAO dao = new AdresDAO();
+	@GET
+	@Produces("application/json")
+	public String getAdressen() {
+		WebshopServiceKlanten service = ServiceProvider.getWebshopServiceKlanten();
+		JsonArrayBuilder jab = Json.createArrayBuilder();
+
+		for (Adres a : service.getAllAdressen()) {
+			JsonObjectBuilder job = Json.createObjectBuilder();
+			job.add("product_id", a.getAdresId());
+			job.add("product_naam", a.getHuisnummer());
+			job.add("product_omschrijving", a.getPlaats());
+			job.add("product_prijs", a.getPostcode());
+			job.add("product_plaatje", a.getStraat());
+
+		}
+		JsonArray array = jab.build();
+		return array.toString();
+	}
+
 
 }
